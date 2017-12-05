265b4a8ea24d5f42802262455544471da8d352ad
win5do <win5do@qq.com>
2017-12-06 00:32:19 +0800

    change path

---
+++ src/benchmark/mock_data.go
@@ -0,0 +1,22 @@
+package benchmark
+
+import (
+	"math/rand"
+)
+
+func generateData(x int) []string {
+	word := "abcdefghijklmnopqrstuvwxyz"
+	data := []string{}
+
+	for x > 0 {
+		x--
+		item := ""
+		for i := 0; i < 4; i++ {
+			pos := rand.Intn(26)
+			item += word[pos:pos+1]
+		}
+		data = append(data, item)
+	}
+
+	return data
+}
