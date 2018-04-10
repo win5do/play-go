e0c3c035460247824bf5334e7305986355c764df
win5do <win5do@qq.com>
2018-04-10 11:06:25 +0800

    realize goroutine limit

--- test/mt.go
+++ test/mt.go
@@ -1,28 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 
-}
-
-func swap(arr []int, i int, j int) {
-	arr[i], arr[j] = arr[j], arr[i]
-}
-
-func insert(arr []int) []int {
-	leng := len(arr)
-
-	for i := 1; i < leng; i++ {
-		temp := arr[i]
-		j := i
-
-		for ; j >= 1 && arr[j-1] > temp; j-- {
-			arr[j] = arr[j-1]
-		}
-
-		arr[j] = temp
-	}
-
-	return arr
 }
