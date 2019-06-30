f2ea4db08b72e1856e4a76dfd4bc0c0dc50241c2
win5do <win5do@qq.com>
2019-06-30 14:14:51 +0800

    sword5

---
+++ src/algorithm/swordToOffer/5/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func replaceBlank(input string) string {
+	originRune := []rune(input)
+
+	originLength := 0
+	countBlank := 0
+
+	for _, v := range originRune {
+		originLength++
+		if v == ' ' {
+			countBlank++
+		}
+	}
+
+	originIndex := originLength - 1
+	newIndex := originIndex + 2*countBlank
+	newRune := append(originRune, make([]rune, 2*countBlank)...)
+
+	for originIndex >= 0 && newIndex > originIndex {
+		if newRune[originIndex] == ' ' {
+			newRune[newIndex] = '0'
+			newRune[newIndex-1] = '2'
+			newRune[newIndex-2] = '%'
+
+			newIndex = newIndex - 2
+		} else {
+			newRune[newIndex] = newRune[originIndex]
+		}
+		originIndex--
+		newIndex--
+	}
+
+	return string(newRune)
+}
+
+func main() {
+	fmt.Println(replaceBlank("we are happy."))
+	fmt.Println(replaceBlank(" we are happy. "))
+	fmt.Println(replaceBlank("we_are_happy."))
+}
