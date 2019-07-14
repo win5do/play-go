33f53336278a6f4df2ba9d6de9b52019ef5ddb6e
win5do <win5do@qq.com>
2019-07-15 01:46:23 +0800

    8

--- src/algorithm/swordToOffer/8/main.go
+++ src/algorithm/swordToOffer/8/main.go
@@ -12,17 +12,15 @@ func GetNext(pNode *BinaryTreeNode) *BinaryTreeNode {
 		return nil
 	}
 
-	var pNext *BinaryTreeNode
-
 	if pNode.Right != nil {
-		pNext = pNode.Right
+		pNext := pNode.Right
 		for pNext.Left != nil {
 			pNext = pNext.Left
 		}
 		return pNext
 	} else if pNode.Parent != nil {
 		pCurrent := pNode
-		pNext = pNode.Parent
+		pNext := pNode.Parent
 		for pNext != nil && pNext.Right == pCurrent {
 			pCurrent = pNext
 			pNext = pCurrent.Parent
