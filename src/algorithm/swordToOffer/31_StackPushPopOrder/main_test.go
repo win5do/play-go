3caf8a472e3e1e77b7198ac7aea0b97405e65079
win5do <win5do@qq.com>
2019-08-26 23:08:57 +0800

    31

---
+++ src/algorithm/swordToOffer/31_StackPushPopOrder/main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsPopOrder(t *testing.T) {
+	cs := []struct {
+		pushOrder []int
+		popOrder  []int
+		expected  bool
+	}{
+		{pushOrder: []int{1, 2, 3, 4, 5}, popOrder: []int{4, 5, 3, 2, 1}, expected: true},
+		{pushOrder: []int{1, 2, 3, 4, 5}, popOrder: []int{4, 3, 5, 1, 2}, expected: false},
+		{pushOrder: []int{1, 2, 3, 4, 5}, popOrder: []int{5, 1, 2, 3, 4}, expected: false},
+		{pushOrder: []int{}, popOrder: []int{}, expected: false},
+		{pushOrder: []int{1}, popOrder: []int{1,2}, expected: false},
+		{pushOrder: []int{1, 1, 1, 1, 1}, popOrder: []int{1, 1, 1, 1, 1}, expected: true},
+		{pushOrder: []int{1, 1, 1, 2, 2}, popOrder: []int{1, 2, 2, 1, 1}, expected: true},
+		{pushOrder: []int{1, 1, 1, 2, 2}, popOrder: []int{2, 2, 2, 1, 1}, expected: false},
+		{pushOrder: []int{1, 1, 1, 2, 2}, popOrder: []int{1, 2, 1, 2, 1}, expected: true},
+		{pushOrder: []int{1, 1, 1, 2, 2}, popOrder: []int{2, 1, 1, 1, 2}, expected: true},
+	}
+
+	for i, v := range cs {
+		r := isPopOrder(v.pushOrder, v.popOrder)
+		assert.Equal(t, v.expected, r, i)
+	}
+}
