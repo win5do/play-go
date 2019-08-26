ab3ca5dff9a92a7c87bfb711fd8262216feec058
win5do <win5do@qq.com>
2019-08-26 14:59:03 +0800

    add top()

--- src/algorithm/swordToOffer/dataStruct/stack/stack.go
+++ src/algorithm/swordToOffer/dataStruct/stack/stack.go
@@ -2,13 +2,15 @@ package stack
 
 type Stack struct {
 	slice []int
+	top   int
 }
 
 func (s *Stack) Push(val int) {
 	s.slice = append(s.slice, val)
+	s.top = val
 }
 
-func (s *Stack) Pop() int{
+func (s *Stack) Pop() int {
 	l := s.Size()
 	if l == 0 {
 		panic("stack empty")
@@ -16,9 +18,19 @@ func (s *Stack) Pop() int{
 
 	val := s.slice[l-1]
 	s.slice = s.slice[:l-1]
+	if l >= 2 {
+		s.top = s.slice[l-2]
+	} else {
+		s.top = 0
+	}
+
 	return val
 }
 
 func (s *Stack) Size() int {
 	return len(s.slice)
+}
+
+func (s *Stack) Top() int {
+	return s.top
 }
