0ac56d197e29027614f48d08835f0b89ee7787a2
win5do <win5do@qq.com>
2019-09-08 18:22:37 +0800

    42

--- src/algorithm/swordToOffer/42_GreatestSumOfSubarrays/main.go
+++ src/algorithm/swordToOffer/42_GreatestSumOfSubarrays/main.go
@@ -3,3 +3,26 @@ package main
 // 面试题42：连续子数组的最大和
 // 题目：输入一个整型数组，数组里有正数也有负数。数组中一个或连续的多个整
 // 数组成一个子数组。求所有子数组的和的最大值。要求时间复杂度为O(n)。
+
+func findMaxSumOfSubArray(arr []int) int {
+	if len(arr) == 0 {
+		return 0
+	}
+
+	curSum := arr[0]
+	maxSum := arr[0]
+
+	for i := 1; i < len(arr); i++ {
+		if curSum < 0 {
+			curSum = arr[i]
+		} else {
+			curSum += arr[i]
+		}
+
+		if curSum > maxSum {
+			maxSum = curSum
+		}
+	}
+
+	return maxSum
+}
--- src/algorithm/swordToOffer/43_NumberOf1/main.go
+++ src/algorithm/swordToOffer/43_NumberOf1/main.go
@@ -3,3 +3,27 @@ package main
 // 面试题43：从1到n整数中1出现的次数
 // 题目：输入一个整数n，求从1到n这n个整数的十进制表示中1出现的次数。例如
 // 输入12，从1到12这些整数中包含1 的数字有1，10，11和12，1一共出现了5次。
+
+// --- method-1 ---
+func numberOf1(n uint64) int {
+	count := 0
+	for n > 0 {
+		if n%10 == 1 {
+			count++
+		}
+		n = n / 10
+	}
+	return count
+}
+
+func numberOf1ToN(n uint64) int {
+	count := 0
+
+	for i := uint64(1); i <= n; i++ {
+		count += numberOf1(i)
+	}
+	return count
+}
+
+// --- method-2 ---
+// TODO
