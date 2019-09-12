83910d0ffff4432a246638b2b2c69b2562979695
win5do <win5do@qq.com>
2019-09-12 18:19:02 +0800

    60 recurse

--- src/algorithm/swordToOffer/60_DicesProbability/main.go
+++ src/algorithm/swordToOffer/60_DicesProbability/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 // 面试题60：n个骰子的点数
 // 题目：把n个骰子扔在地上，所有骰子朝上一面的点数之和为s。输入n，打印出s
 // 的所有可能的值出现的概率。
+const NUMBER = 6
+
+func printProbability_recurse(n int) {
+	if n < 1 {
+		return
+	}
+
+	max := n * 6
+	result := make([]int, max-NUMBER+1)
+
+	probability(n, n, 0, result)
+
+	total := math.Pow(NUMBER, float64(n))
+
+	for i, v := range result {
+		fmt.Printf("%d: %f\n", i+n, float64(v)/total)
+	}
+}
+
+func probability(original, rest, sum int, result []int) {
+	if rest < 1 {
+		result[sum-original]++
+		return
+	}
+
+	for i := 1; i <= NUMBER; i++ {
+		probability(original, rest-1, sum+i, result)
+	}
+}
---
+++ src/algorithm/swordToOffer/60_DicesProbability/main_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestFuncRecurse(t *testing.T) {
+	printProbability_recurse(6)
+}
