6d7aadf484af0aecccacad98072bae33cea51207
win5do <win5do@qq.com>
2019-09-18 11:00:41 +0800

    add comment

--- src/algorithm/swordToOffer/68_CommonParentInTree/main.go
+++ src/algorithm/swordToOffer/68_CommonParentInTree/main.go
@@ -4,6 +4,13 @@ import (
 	"playGo/src/algorithm/swordToOffer/dataStruct/tree"
 )
 
+// 面试题68：树中两个结点的最低公共祖先
+// 题目：输入两个树结点，求它们的最低公共祖先。
+
+// 思路：用DFS或BFS找到根节点到该节点的路径，
+// 再找到两个路径中最后一个相同节点
+
+// --- dfs ---
 func getCommonParent_dfs(head, a, b *tree.Tree) (parent *tree.Tree) {
 	if head == nil || a == nil || b == nil {
 		return nil
@@ -17,6 +24,7 @@ func getCommonParent_dfs(head, a, b *tree.Tree) (parent *tree.Tree) {
 	return getLastCommonParent(aList, bList)
 }
 
+// --- bfs ---
 func getCommonParent_bfs(head, a, b *tree.Tree) (parent *tree.Tree) {
 	if head == nil || a == nil || b == nil {
 		return nil
@@ -30,6 +38,7 @@ func getCommonParent_bfs(head, a, b *tree.Tree) (parent *tree.Tree) {
 	return getLastCommonParent(aList, bList)
 }
 
+// --- common ---
 func getLastCommonParent(aList, bList []*tree.Tree) (common *tree.Tree) {
 	for k, v := range aList {
 		// 最后一个元素是a,b, k最多为n-2
