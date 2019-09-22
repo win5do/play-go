41d546e8985d942e1ca657f9653f92d8c2433894
win5do <win5do@qq.com>
2019-09-23 00:06:54 +0800

    heapSort

--- src/algorithm/sortAlgorithm/heap_sort.go
+++ src/algorithm/sortAlgorithm/heap_sort.go
@@ -1,41 +1,40 @@
 package sortAlgorithm
 
-import "fmt"
-
 func heapSort(arr []int) []int {
-	buildMaxHeap(arr)
+	l := len(arr)
 
-	fmt.Println(arr)
+	// 构建大顶堆
+	for i := l/2 - 1; i >= 0; i-- {
+		adjustHeap(arr, i)
+	}
 
-	for i := len(arr) - 1; i > 0; i-- {
+	// 将根节点(最大元素)与末尾元素互换，对剩余数组构建大顶堆
+	for i := l - 1; i > 0; i-- {
 		swap(arr, 0, i)
-		buildMaxHeap(arr[:i])
+		adjustHeap(arr[:i], 0)
 	}
 
 	return arr
 }
 
-func buildMaxHeap(arr []int) {
-	leng := len(arr)
-	if leng <= 1 {
-		return
-	}
+// 如果arr[k]比左右子节点小则下沉，直至叶子节点
+func adjustHeap(arr []int, k int) {
+	l := len(arr)
 
-	for i := (leng - 1) / 2; i >= 0; i-- {
-		max := i
-		left := 2*i + 1
-		right := 2*i + 2
+	temp := arr[k]
 
-		if left < leng && arr[max] < arr[left] {
-			max = left
+	for i := 2*k + 1; i < l; i = 2*i + 1 {
+		if i+1 < l && arr[i+1] > arr[i] {
+			i++
 		}
 
-		if right < leng && arr[max] < arr[right] {
-			max = right
-		}
-
-		if i != max {
-			swap(arr, i, max)
+		if (arr[i]) > temp {
+			arr[k] = arr[i]
+			k = i
+		} else {
+			break
 		}
 	}
+
+	arr[k] = temp
 }
