d41b42e8d10063417e644a18fc62a1cf66073a16
win5do <win5do@qq.com>
2021-03-08 17:28:30 +0800

    589

---
+++ src/algorithm/leetcode/589-n-ary-tree-preorder-traversal/recurse/main.go
@@ -0,0 +1,33 @@
+package recurse
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+func preorder(root *Node) []int {
+	var ans []int
+
+	preorderCore(root, &ans)
+
+	return ans
+}
+
+func preorderCore(node *Node, ans *[]int) {
+	if node == nil {
+		return
+	}
+	*ans = append(*ans, node.Val)
+
+	for _, v := range node.Children {
+		preorderCore(v, ans)
+	}
+}
